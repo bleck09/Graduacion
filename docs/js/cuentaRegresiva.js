54e06d08e49a91957092752327de9e42f9abef23
Blady-87 <cmv2018174@est.univalle.edu>
2025-04-02 00:17:49 -0400

    Proyecto terminado

--- docs/js/cuentaRegresiva.js
+++ docs/js/cuentaRegresiva.js
@@ -1,4 +1,4 @@
-// Fecha del acto: 5 de abril de 2025 a las 14:00:00
+
 const graduationDate = new Date("April 5, 2025 14:00:00").getTime();
 
 const countdownInterval = setInterval(() => {
@@ -10,13 +10,13 @@ const countdownInterval = setInterval(() => {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Actualiza los elementos del contador en tu HTML
+  
   document.getElementById("days").textContent = days;
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
   document.getElementById("seconds").textContent = seconds;
 
-  // Si la cuenta regresiva llega a 0, muestra un mensaje
+  
   if (distance < 0) {
     clearInterval(countdownInterval);
     document.querySelector(".section3-content").innerHTML = "<h1>¡Es el día del acto!</h1>";
